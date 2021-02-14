81371c0a02d44f9983f7d1aae37afb9bf0f275b4
Viktor Repin <hiddenstmail@gmail.com>
2021-02-14 23:38:31 +0700

    Remove unnecessary directories

--- Base/third_party_api/api_wrapper.py
+++
@@ -1,9 +0,0 @@
-
-
-class ApiWrapper:
-    """Base class for APIWrappers."""
-    def make_query(self, parameters: str):
-        raise RuntimeError("Method make_query() is not overridden!")
-
-    def get_result(self):
-        raise RuntimeError("Method get_result() is not overridden!")
--- Base/views/Querys.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import authentication, permissions
-
-
-class Querys(APIView):
-    def get(self, request, format=None):
-        
-        return Response(status=204)
