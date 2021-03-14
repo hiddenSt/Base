b8f0e170a6a0bde440e4575f2ea216bcd31622a0
boyboyjoy <moximmorozov@yandex.ru>
2021-03-14 20:03:17 +0400

    fix top_request_test bag

--- Base/tests/test_top_requests.py
+++ Base/tests/test_top_requests.py
@@ -1,4 +1,4 @@
-from unittest import TestCase
+from django.test import TestCase
 
 from django.contrib.auth.models import User
 
@@ -7,17 +7,19 @@ from Base.services.top_requests import get_top
 
 
 class TopRequestsTest(TestCase):
+    def setUp(self):
+        self.userInstance = User.objects.create(username='zxc')
 
     def test_top_requests_logic(self):
         request_dict = {}
-        RequestModel.objects.all().delete()
-        userInstance = User.objects.all()[0]
-        RequestModel.objects.create(user=userInstance, text='123')
+        RequestModel.objects.create(user=self.userInstance, text='123')
         request_dict.setdefault('123', 1)
         for i in range(2):
-            RequestModel.objects.create(user=userInstance, text='12345')
+            RequestModel.objects.create(user=self.userInstance, text='12345')
         request_dict.setdefault('12345', 2)
         for i in range(5):
-            RequestModel.objects.create(user=userInstance, text='123456789')
+            RequestModel.objects.create(user=self.userInstance,
+                                        text='123456789')
         request_dict.setdefault('123456789', 5)
+        print(get_top(), request_dict)
         self.assertEqual(get_top(), request_dict)
