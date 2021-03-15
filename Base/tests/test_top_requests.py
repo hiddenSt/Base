22be5fe0671343724aeadf5d49b9725ffba3e065
boyboyjoy <moximmorozov@yandex.ru>
2021-03-15 10:45:44 +0400

    micro fix in top_request

--- Base/tests/test_top_requests.py
+++ Base/tests/test_top_requests.py
@@ -16,11 +16,11 @@ class TopRequestsTest(TestCase):
         TEXT_3 = '123456789'
         request_dict = {}
         RequestModel.objects.create(user=self.userInstance, text=TEXT_1)
-        request_dict.setdefault('123', 1)
+        request_dict.setdefault(TEXT_1, 1)
         for i in range(2):
             RequestModel.objects.create(user=self.userInstance, text=TEXT_2)
-        request_dict.setdefault('12345', 2)
+        request_dict.setdefault(TEXT_2, 2)
         for i in range(5):
             RequestModel.objects.create(user=self.userInstance, text=TEXT_3)
-        request_dict.setdefault('123456789', 5)
+        request_dict.setdefault(TEXT_3, 5)
         self.assertEqual(get_top(), request_dict)
