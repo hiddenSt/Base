f505785bf3b288afe6e9f9887acf578897d023e9
Viktor Repin <hiddenstmail@gmail.com>
2021-04-22 00:00:28 +0700

    Returns token

--- authentication/views.py
+++ authentication/views.py
@@ -4,6 +4,8 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .serializers import RegistrationSerializer
+from .serializers import LoginSerializer
+
 from .renderers import UserJSONRenderer
 
 
@@ -20,5 +22,19 @@ class RegistrationAPIView(APIView):
         serializer.save()
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class LoginAPIView(APIView):
+    permission_classes = (AllowAny, )
+    renderer_classes = (UserJSONRenderer, )
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        user = request.data.get('user', {})
+
+        serializer = self.serializer_class(data=user)
+        serializer.is_valid(raise_exception=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
