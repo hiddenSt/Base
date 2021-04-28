99376c4e9cd722b715cedece4ab896ecfeeb9ef7
boyboyjoy <moximmorozov@yandex.ru>
2021-04-28 22:07:13 +0430

    add youtube tests

---
+++ Base/youtube_api/tests/test_youtube_api_wrapper.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+from Base.youtube_api.youtube_api_wrapper import YoutubeApiWrapper
+
+
+class YoutubeApiWrapperTestCase(TestCase):
+    TEST_CHANNEL_TITLE = 'Tusur'
+
+    def setUp(self):
+        self.wrapper = YoutubeApiWrapper()
+
+    def test_search_channels(self):
+        self.wrapper.search_youtube_channels_by_keyword(self.TEST_CHANNEL_TITLE)
+        self.assertTrue(self.wrapper.youtube_channel_list_data)
+
