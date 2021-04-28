52c4ab79ce00b6575e5d62d3a13c1ca6d9d11d36
FieldmarshalDub <kirh09764@gmail.com>
2021-04-28 22:41:24 +0700

    Изменены тесты

--- TelegramApiWrapper/tests/test_telegram_wrapper.py
+++ TelegramApiWrapper/tests/test_telegram_wrapper.py
@@ -15,11 +15,21 @@ class WrapperTestCase(TestCase):
         self.telegram = TelegramApiWrapper()
 
     def test_search_channels(self):
-        query = "python"
+        query = "Python"
         result = self.telegram.search_channels(query)
-        self.assertTrue(result[0]['title'].find(query))
+        result_b = False
+        if result[0]['title'].find(query) != -1:
+            result_b = True
+        self.assertTrue(result_b)
 
-    def test_create_dict(self):
+    def test_can_call_method_create_dict(self):
+        try:
+            channel = Channel()
+            result_dict = self.telegram.create_dict(channel, channel.channel_info)
+        except BaseException:
+            self.fail("create_dict raised ThatException")
+
+    def test_can_create_dict(self):
         correct_dict = {'title': "title",
                         'members': "90000",
                         'link': "t.me/username",
