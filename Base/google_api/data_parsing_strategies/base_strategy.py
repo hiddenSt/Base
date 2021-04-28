e7127bd6fa472df4ddeba4bcfc2a22c72fb72b6e
boyboyjoy <moximmorozov@yandex.ru>
2021-04-28 19:25:13 +0430

    add youtube api

--- Base/google_api/data_parsing_strategies/base_strategy.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class DataParsingStrategy(ABC):
-    @abstractmethod
-    def parse_data(self, data):
-        pass
--- Base/google_api/google_api.py
+++ Base/google_api/google_api.py
@@ -1,4 +1,6 @@
+import json
 import requests
+import environ
 
 
 class GoogleBookApiWrapper:
@@ -6,11 +8,30 @@ class GoogleBookApiWrapper:
                                      'volumes?q={0}&key={1}&maxResults={2}'}
     MAX_RESPONSES_NUMBER = 5
 
-    def __init__(self, api_key):
-        self.API_KEY = api_key
+    def __init__(self):
+        ENV = environ.Env()
+        environ.Env.read_env()
+        self.API_KEY = ENV('GOOGLE_API_KEY')
 
-    def get_book_by_title(self, book_title, data_parsing_strategy):
+    def get_book_by_title(self, book_title):
         response = requests.get(
             url=GoogleBookApiWrapper.URL_PATTERNS['searching_url'].format(
                 book_title, self.API_KEY, self.MAX_RESPONSES_NUMBER))
-        return data_parsing_strategy.parse_data(response.content)
+        return self.parse_data(response.content)
+
+    def parse_data(self, book_data):
+        book_data = json.loads(book_data)
+        all_books = []
+        for item in book_data['items']:
+            volume_info = item['volumeInfo']
+            data = {'title': volume_info.get('title'),
+                    'author': volume_info.get('authors'),
+                    'publisher': volume_info.get(
+                        'publisher'), 'publishedDate': volume_info.get(
+                    'publishedDate'),
+                    'pageCount': volume_info.get('pageCount'),
+                    'rating': volume_info.get('averageRating'), 'annotation':
+                        volume_info.get('description')
+                    }
+            all_books.append(data)
+        return json.dumps(all_books)
