3ecc7c3ec08a33f4839ce6e34a56d71d70d65448
Viktor O. Repin <vrepin@tomsksoft.com>
2021-05-06 08:29:25 +0700

    Fix linter errors

--- Base/youtube_api/tests/test_youtube_api_wrapper.py
+++ Base/youtube_api/tests/test_youtube_api_wrapper.py
@@ -11,4 +11,3 @@ class YoutubeApiWrapperTestCase(TestCase):
     def test_wrapper_get_response(self):
         self.wrapper.search_youtube_channels_by_keyword(self.TEST_CHANNEL_TITLE)
         self.assertTrue(self.wrapper.youtube_channel_list_data)
-
